8ae9326ba075000c8c0843409449ba54a8463b98
mmcdan13 <micahmcdaniel0913@gmail.com>
2022-07-14 17:36:42 -0400

    Added Retiring Titles table and export

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,7 +1,6 @@
--- Create Retiring_titles table
-
+-- CREATE RETIREMENT TITLES table 
 SELECT employees.emp_no, first_name, last_name, title, from_date, to_date
-INTO retiring_titles
+INTO retirement_titles
 FROM employees
 left join titles 
 on employees.emp_no = titles.emp_no
@@ -9,12 +8,22 @@ where (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 order by emp_no;
 
 -- Use Dictinct with Orderby to remove duplicate rows
+--CREATE UNIQUE TITLES table
 SELECT DISTINCT ON (emp_no) emp_no,
 first_name,
 last_name,
 title
 
 INTO unique_titles
-FROM retiring_titles
+FROM retirement_titles
 WHERE to_date = '9999-01-01'
 ORDER BY emp_no, to_date DESC;
+
+-- Number of Employees per Most Recent Job Title
+-- CREATE RETIRING TITLES table
+
+SELECT count(title), title
+INTO retiring_titles
+FROM unique_titles
+group by title
+order by count(title) desc;
