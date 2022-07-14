d062088b494a8ee1c662e748fc1ffe7f88427907
mmcdan13 <micahmcdaniel0913@gmail.com>
2022-07-14 17:16:42 -0400

    distinct employee with most recent title

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -7,3 +7,14 @@ left join titles
 on employees.emp_no = titles.emp_no
 where (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 order by emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+
+INTO unique_titles
+FROM retiring_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
